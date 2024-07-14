793a65141fe983b093b3535c9517dfbb32079abd
willbsp <github-wills.12zz7@aleeas.com>
2024-07-14 12:47:05 +0100

    More configuration

--- machines/hal/configuration.nix
+++ machines/hal/configuration.nix
@@ -20,6 +20,14 @@
   # Networking
   networking.hostName = "hal"; # Define your hostname.
   networking.networkmanager.enable = true;
+  networking = {
+    interfaces.eno1 = {
+      ipv4.addresses = [{
+        address = "192.168.10.4";
+        prefixLength = 24;
+      }];
+    };
+  };
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false; # prevent nixos-rebuild failing
   services.tailscale = {
     enable = true;
@@ -58,6 +66,10 @@
     device = "/dev/disk/by-uuid/61992827-20ca-4322-ae3d-ad7226317541";
     fsType = "ext4";
   };
+  fileSystems."/mnt/ks-int" = {
+    device = "/dev/disk/by-uuid/13bd81ee-f89d-4fa3-9e04-51412d920e94";
+    fsType = "ext4";
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.defaultUserShell = pkgs.zsh;
@@ -68,7 +80,10 @@
   };
 
   # Docker
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    package = pkgs.docker_25;
+  };
 
   programs.zsh = {
     enable = true;
@@ -99,6 +114,7 @@
   environment.systemPackages = with pkgs; [
     git
     docker-compose
+    borgbackup
   ];
 
   # Enable the OpenSSH daemon.
