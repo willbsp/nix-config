6cbafb76108870e7b913ed0ad4f8f4c68e4d1e68
willbsp <github-wills.12zz7@aleeas.com>
2024-07-22 10:23:18 +0100

    Remove kitty tabs

--- hm-modules/kitty/kitty.nix
+++ hm-modules/kitty/kitty.nix
@@ -9,9 +9,19 @@
       programs.kitty = {
         enable = true;
         theme = "Catppuccin-Frappe";
+          keybindings = {
+            # Unmap tab shortcuts, prefer using Sways tab layout
+            "ctrl+shift+right" = "";
+            "ctrl+shift+left" = "";
+            "ctrl+tab" = "";
+            "ctrl+shift+tab" = "";
+            "ctrl+shift+q" = "";
+            "ctrl+shift+." = "";
+            "ctrl+shift+," = "";
+            "ctrl+shift+alt+t" = "";
+          };
         settings = {
           cursor_shape = "beam";
-          tab_bar_style = "fade";
           tab_bar_align = "left";
           tab_bar_min_tabs = 1;
           wayland_titlebar_color = "background";
@@ -20,6 +30,7 @@
           initial_window_width = "127c";
           initial_window_height = "50c";
           confirm_os_window_close = 0;
+          tab_bar_style = "hidden";
         };
       };
     };
